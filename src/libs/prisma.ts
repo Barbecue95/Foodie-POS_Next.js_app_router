5969f29c3623be0bea56bd548ed042edeb1e8336
Kaung Htet ZIn <kaunghtetzin@Kaungs-MacBook-Air.local>
2024-10-12 14:56:45 +0630

    Fix Prisma File

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -4,13 +4,4 @@ declare global {
   var prisma: PrismaClient;
 }
 
-export let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+export const prisma = new PrismaClient();
